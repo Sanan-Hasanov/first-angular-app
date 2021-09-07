8e44ca12e410560010c58a4fcee8905c023257cb
Sinan Hasanov <hesenov9669@gmail.com>
2021-09-08 02:11:00 +0300

    BS4-ve-toastr-yukleme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'first-app';
+
+  constructor(private toastr: ToastrService) { }
+  ngOnInit(): void {
+    this.showError();
+  }
+
+  showSuccess() {
+    this.toastr.success('Salam Dünya!', 'Giriş baçarılı!');
+  }
+  showError(){
+    this.toastr.error('Bu bir hata mesaji!', 'Hata mesaji basligi!')
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ToastrModule } from 'ngx-toastr';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot({
+      timeOut: 5000,
+      progressBar: true,
+      easing: "ease-in",
+      closeButton: true,
+      progressAnimation: 'decreasing', 
+      preventDuplicates: true,
+      positionClass: "toast-bottom-left"
+    }), // ToastrModule added
   ],
   providers: [],
   bootstrap: [AppComponent]
